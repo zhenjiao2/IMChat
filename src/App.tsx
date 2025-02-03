cd1e6a50586692acbd6a37d995486132d94aa363
Zhen Jiao <zhenjiao2@outlook.com>
2025-02-03 20:49:37 +0800

    add chat app

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { TUILogin } from '@tencentcloud/tui-core';
+import { Chat, ConversationList, ChatSetting } from '@tencentcloud/chat-uikit-react';
+import { TUIConversationService } from '@tencentcloud/chat-uikit-engine';
+import { UIKitProvider } from '@tencentcloud/uikit-base-component-react';
+import '@tencentcloud/chat-uikit-react/dist/esm/index.css';
+import {
+  TUIStore,
+  StoreName,
+  IMessageModel,
+} from '@tencentcloud/chat-uikit-engine';
+
+const config = {
+  SDKAppID: Number(process.env.SDK_APP_ID), 
+  userID: String(process.env.USER_ID), 
+  userSig: String(process.env.USER_SIG), 
+}
+
+// TUILogin.login({
+//   ...config,
+//   useUploadPlugin: true
+// }).then(() => {
+//   openChat();
+// }).catch(() => { });
+
+TUILogin.login({
+  ...config,
+  useUploadPlugin: true
+})
+.then(() => {})
+.catch(() => {});
+
+function openChat() {
+  // 1v1 chat: conversationID = 
+  // group chat: conversationID = `GROUP${groupID}`
+  const userID = 'test2'; // userID: 消息接收方的 userID
+  const conversationID = `C2C${userID}`;
+  TUIConversationService.switchConversation(conversationID);
+}
+
+export default function App() {
+  // language support en-US(default) / zh-CN / ja-JP / ko-KR / zh-TW
+  // theme support light(default) / dark
+  return (
+    <div style={{display: 'flex', height: '100vh'}}>
+      <UIKitProvider language='en-US' theme='light'>
+        <div style={{ display: 'flex', maxWidth: '30%'}}>
+          <ConversationList />
+        </div>
+        <Chat />
+        <ChatSetting />
+      </UIKitProvider>
+    </div>     
+    );
+}
